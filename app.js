0b4f347d7c755a730dc711f35a6b7505128de745
debjoitee biswas <133802416+debjoitee@users.noreply.github.com>
2026-03-25 15:01:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-// app.js — PUBLIC SITE (same Firebase logic, beautiful new UI)
-import { db } from "./firebase.js";
-import { collection, getDocs }
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, getDocs }
   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOK66hRh1s6U4P8xuuim9zup6tr7maaV8",
+  authDomain: "builder-store-19a9f.firebaseapp.com",
+  projectId: "builder-store-19a9f",
+  storageBucket: "builder-store-19a9f.firebasestorage.app",
+  messagingSenderId: "1004045770027",
+  appId: "1:1004045770027:web:ba140763bda6ab007cf96e"
+};
+const app = initializeApp(firebaseConfig);
+const db  = getFirestore(app);
 
 let allProducts = [];
 
