3b50ea097635c28f4154cc5167f3c73dee74e8a0
debjoitee biswas <133802416+debjoitee@users.noreply.github.com>
2026-03-25 14:49:43 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,14 @@
-
-
-// firebase.js — paste YOUR real Firebase config values here
-import { initializeApp }
+// firebase.js
+import { initializeApp } 
   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore }
+import { getFirestore } 
   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAuth }
+import { getAuth } 
   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getStorage }
+import { getStorage } 
   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCOK66hRh1s6U4P8xuuim9zup6tr7maaV8",
   authDomain: "builder-store-19a9f.firebaseapp.com",
@@ -18,8 +17,9 @@ const firebaseConfig = {
   messagingSenderId: "1004045770027",
   appId: "1:1004045770027:web:ba140763bda6ab007cf96e"
 };
+
 const app = initializeApp(firebaseConfig);
 
-export const db      = getFirestore(app);
-export const auth    = getAuth(app);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
 export const storage = getStorage(app);
